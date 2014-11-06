4b2bfe38b0ac6911d87460f93ba814189b4d5622
Alexander Selzer <aselzer3@gmail.com>
2014-11-06 22:51:16 +0100

    CORS

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.set("secret", config.secret || "seeeecret...not")
 app.use(bodyParser.json())
 //app.use(passport.initialize())
 app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   logger.info(req.method + " " + req.path)
   next()
 })
